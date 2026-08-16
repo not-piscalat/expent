62d54712a80517a4ec2938e4b8a6de8214a6a405
not-piscalat <2justmonika1@gmail.com>
2026-08-16 15:45:53 +0800

    Bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.expent.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
